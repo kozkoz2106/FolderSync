e4f5cfd423e7c16c12436618f9fbe8f01679093c
Aaroh Purani <aaroskey@gmail.com>
2025-05-27 13:42:59 +1000

    installed pyusb - needed to access usb devices

--- fileopps.py
+++ fileopps.py
@@ -1,32 +1,24 @@
 import shutil
 import os
-import subprocess
+#import subprocess
+import pyusb
 #from tkinter import *
 
 def find_folder(start_dir='/', folder_name='Images'):
-    print("a")
     for root, dirs, files in os.walk(start_dir):
-        print('b')
         for dir in dirs:
-            print('c')
             if dir == folder_name:
-                print('d')
                 full_path = os.path.join(root, dir)
                 try:
-                    print('e')
-                    print(full_path + folder_name)
-                    subprocess.run(['open', full_path])
-                    return 
+                    return full_path
                 except Exception as e:
-                    print('f')
                     print(f"Error opening {full_path}: {e}")
 
 def clone():
     source = input("Enter a source folder \n")
     destination = input("Enter a destination folder\n")
-
-    sourceDIR = "/Users/aarohpurani/Desktop/Images/pc"
-    destinationDIR = "/Users/aarohpurani/Desktop/Images/usb"   
+    sourceDIR = find_folder('/Users/aarohpurani/', source)
+    destinationDIR = find_folder('/Users/aarohpurani/', destination)
 
     sourceDIR_name = (os.path.basename(sourceDIR))
     destinationDIR_name = (os.path.basename(destinationDIR))
@@ -37,8 +29,8 @@ def clone():
 def push():
     source = input("Enter a source folder \n")
     destination = input("Enter a destination folder\n")
-    sourceDIR = "/Users/aarohpurani/Desktop/Images/test2"
-    destinationDIR = "/Users/aarohpurani/Desktop/Images/test1"
+    sourceDIR = find_folder('/Users/aarohpurani/', source)
+    destinationDIR = find_folder('/Users/aarohpurani/', destination)
 
     sourceDIR_name = (os.path.basename(sourceDIR))
     destinationDIR_name = (os.path.basename(destinationDIR))
