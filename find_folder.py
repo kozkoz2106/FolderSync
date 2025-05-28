3155a57451618799640e7d1c9b1fb7f5bfa0f33f
Aaroh Purani <aaroskey@gmail.com>
2025-05-28 22:16:35 +1000

    cleaned up some of the folder retrieval and some minor message formatting

--- find_folder.py
+++ find_folder.py
@@ -1,16 +1,13 @@
 import os
 
 # def find_folder_external(start_dir='/', folder_name ='Images'):
-#     return 0
+    # for root, dirs
 
 def find_folder_local(start_dir='/', folder_name='Images'):
     for root, dirs, files in os.walk(start_dir):
         for dir in dirs:
             if dir == folder_name:
                 full_path = os.path.join(root, dir)
-                try:
-                    return full_path
-                except Exception as e:
-                    print(f"Error opening {full_path}: {e}")
+                return full_path
                     
-    #find_folder_external('/Volumes', folder_name)
+    # find_folder_external('/Volumes', folder_name)
--- operations.py
+++ operations.py
@@ -5,26 +5,9 @@ import os
 #from tkinter import *
 from find_folder import *
 
-#def find_folder_external(start_dir='/', folder_name ='Images'):
-   # return 0
-
-# def find_folder_local(start_dir='/', folder_name='Images'):
-#     for root, dirs, files in os.walk(start_dir):
-#         for dir in dirs:
-#             if dir == folder_name:
-#                 full_path = os.path.join(root, dir)
-#                 try:
-#                     print(full_path)
-#                     return full_path
-                
-#                 except Exception as e:
-#                     print(f"Error opening {full_path}: {e}")
-                    
-    #find_folder_external('/Volumes', folder_name)
-
 def clone():
-    source = input("Enter a source folder \n")
-    destination = input("Enter a destination folder\n")
+    source = input("Enter a source folder: ")
+    destination = input("Enter a destination folder: ")
     sourceDIR = find_folder_local('/Users/aarohpurani/', source)
     destinationDIR = find_folder_local('/Users/aarohpurani/', destination)
 
@@ -35,8 +18,8 @@ def clone():
     return "The contents of " + sourceDIR_name + " have been copied into " + destinationDIR_name
 
 def push():
-    source = input("Enter a source folder \n")
-    destination = input("Enter a destination folder\n")
+    source = input("Enter a source folder: ")
+    destination = input("Enter a destination folder: ")
     sourceDIR = find_folder_local('/Users/aarohpurani/', source)
     destinationDIR = find_folder_local('/Users/aarohpurani/', destination)
 
