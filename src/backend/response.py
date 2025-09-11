a89dfa93cd3320fad7fbff7cd0065e156852f4bf
Aaroh Purani <aaroskey@gmail.com>
2025-09-11 11:22:13 +1000

    committing change to help

--- src/backend/response.py
+++ src/backend/response.py
@@ -2,7 +2,7 @@ from operations import *
 
 def commands(command):
     if command == "--help":
-        print("gitfile --sync: Copies all data from a remote repository into an empty folder\ngitfile --pull [folder]: Updates local repository with changes from remote repository \n")
+        print("gitfile --sync: Copies all data from a remote repository into an empty folder. Two-way syncing may be enabled. \n")
     elif command == "--sync":
         message = sync()
         print(message)
