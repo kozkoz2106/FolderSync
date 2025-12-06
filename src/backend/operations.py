43c0413d73976e2dbd6473189d5c71db2f90e0c8
Aaroh Purani <aaroskey@gmail.com>
2025-12-07 10:36:23 +1100

    Now allowing multiple folders to sync at once

--- src/backend/operations.py
+++ src/backend/operations.py
@@ -3,11 +3,13 @@ import os
 from find_folder import *
 
 # HELPER FUNCTIONS
-def syncOneWay(sourceDIR, destinationDIR, sourceDIR_name, destinationDIR_name):
-    copytree(sourceDIR, destinationDIR, dirs_exist_ok=True)
-    return "The contents of " + sourceDIR_name + " have been copied into " + destinationDIR_name + "."
+def syncOneWay(source_list, destinationDIR, destinationDIR_name):
+    for item in source_list: 
+        copytree(item, destinationDIR, dirs_exist_ok=True)
 
-def syncBothWay(sourceDIR, destinationDIR, sourceDIR_name, destinationDIR_name):
+    return "The contents of have been copied into " + destinationDIR_name + "."
+
+def syncBothWay(sourceDIR, destinationDIR, source_list, destinationDIR_name):
     copytree(sourceDIR, destinationDIR, True)
     copytree(destinationDIR, sourceDIR, True)
     return "The contents of " + sourceDIR_name + " and " + destinationDIR_name + " have been synced both ways."
@@ -15,14 +17,21 @@ def syncBothWay(sourceDIR, destinationDIR, sourceDIR_name, destinationDIR_name):
 # PRIMARY FUNCTIONS
 def sync():
 
-    source = input("Enter a source folder: ")
     home_path = os.path.expanduser("~")
-    sourceDIR = find_folder(home_path, source)
+    source_list = []
 
-    if sourceDIR == 'not found':
-        print('Folder ' + source + ' not found')
-        exit(0)
-    sourceDIR_name = (os.path.basename(sourceDIR))
+    print("Enter a source folder(s): ")
+
+    while (input != 'done'):
+        source = input()
+        if source == 'done':
+            break
+        sourceDIR = find_folder(home_path, source)
+        if sourceDIR == 'not found':
+            print('Folder ' + source + ' not found')
+        else:
+            print(source + ' selected')
+            source_list.append(sourceDIR)
 
     destination = input("Enter a destination folder: ")
     destinationDIR = find_folder(home_path, destination)
@@ -38,6 +47,6 @@ def sync():
         sync_type = input("Not a valid command. Type 'yes' or 'no': ")
     
     if sync_type == 'no':
-        return syncOneWay(sourceDIR, destinationDIR, sourceDIR_name, destinationDIR_name)
+        return syncOneWay(source_list, destinationDIR, destinationDIR_name)
     elif sync_type == 'yes':
-        return syncBothWay(sourceDIR, destinationDIR, sourceDIR_name, destinationDIR_name)
+        return syncBothWay(source_list, destinationDIR, destinationDIR_name)
