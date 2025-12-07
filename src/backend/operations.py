29f1bb354dc1f5aae8cc7fc6c42160099b5a3098
Aaroh Purani <aaroskey@gmail.com>
2025-12-07 21:01:19 +1100

    More thorough two-way syncing, where multiple destinations and multiple sources can be synced at once

--- src/backend/operations.py
+++ src/backend/operations.py
@@ -3,21 +3,28 @@ import os
 from find_folder import *
 
 # HELPER FUNCTIONS
-def syncFiles(source_list, destinationDIR, destinationDIR_name, one_way):
-    for item in source_list: 
-        copytree(item, destinationDIR, dirs_exist_ok=True)
+def syncFiles(source_list, dest_list, one_way):
+    print('a')
+    for source in source_list: 
+        for dest in dest_list:
+            copytree(source, dest, dirs_exist_ok=True)
     if one_way == False:
-        return "The contents of your sources have been copied into " + destinationDIR_name + "."
+        return "The contents of your source(s) have been copied into your destination(s)"
     else:  
-        for item in source_list:
-            copytree(destinationDIR, item, dirs_exist_ok=True)
-        return "The contents of your sources and " + destinationDIR_name + " have been synced both ways."
+        for source in source_list:
+            for dest in dest_list:
+                copytree(dest, source, dirs_exist_ok=True)
+        for source in source_list:
+            for dest in dest_list:
+                copytree(source, dest, dirs_exist_ok=True)
+        return "The contents of your source(s) and destination(s) have been synced both ways."
 
 # PRIMARY FUNCTIONS
 def sync():
 
     home_path = os.path.expanduser("~")
     source_list = []
+    dest_list = []
 
     print("Enter a source folder(s): ")
 
@@ -32,13 +39,22 @@ def sync():
             print(source + ' selected')
             source_list.append(sourceDIR)
 
-    destination = input("Enter a destination folder: ")
-    destinationDIR = find_folder(home_path, destination)
+    print("Enter a destination folder: ")
 
-    if destinationDIR == 'not found':
-        print('Folder ' + destination + ' not found')
-        exit(0)
-    destinationDIR_name = (os.path.basename(destinationDIR))
+    while (input != 'done'):
+        print('a')
+        dest = input()
+        if dest == 'done':
+            break
+        destDIR = find_folder(home_path, dest)
+        if destDIR == 'not found':
+            print('b')
+            print('Folder ' + dest + ' not found')
+        else:
+            print('c')
+            print(dest + ' selected')
+            dest_list.append(destDIR)
+        print('d')
 
     one_way = False
     sync_type = input("Two way syncing? Type 'yes' or 'no': ")
@@ -47,7 +63,8 @@ def sync():
         sync_type = input("Not a valid command. Type 'yes' or 'no': ")
     
     if sync_type == 'no':
-        return syncFiles(source_list, destinationDIR, destinationDIR_name, one_way)
+        return syncFiles(source_list, dest_list, one_way)
     else:
         one_way = True
-        return syncFiles(source_list, destinationDIR, destinationDIR_name, one_way)
+        return syncFiles(source_list, dest_list, one_way)
+    
